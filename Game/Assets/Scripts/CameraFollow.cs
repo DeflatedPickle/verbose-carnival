0f1b14a4267917d0d50fe5b06e74fde911eba6ef
DeflatedPickle <DeflatedPickle@gmail.com>
2019-03-18 06:54:59 +0000

    Added a camera script

---
+++ Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform Target;
+
+	public float MovementSpeed;
+	public Vector3 OffsetVector;
+
+	private Vector3 _velocity;
+	
+	private void LateUpdate() {
+		transform.position = Vector3.SmoothDamp(transform.position, Target.position + OffsetVector, ref _velocity, MovementSpeed);
+	}
+}
